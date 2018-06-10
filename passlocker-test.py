a6111b063991e565645aadef2c6c9a6078ce979d
wanjiiru <liz240@live.com>
2018-06-10 17:48:54 +0300

    add setup, init aand teardown tests

--- passlocker-test.py
+++ passlocker-test.py
@@ -2,3 +2,36 @@ from passLocker import Credential
 from passLocker import UserData
 import pyperclip
 import unittest
+
+
+class Testcredential(unittest.TestCase):
+    '''
+    '''
+
+
+    def setUp(self):
+        '''
+        set up structure before every test
+        '''
+        self.new_creds = Credential("liz","pass")
+
+
+
+    def tearDown(self):
+        '''
+        clean up after running each test
+        '''
+        Credential.cred_list = []
+
+
+    def test_init(self):
+        '''
+        Test for case initialization
+        '''
+        self.assertEqual(self.new_creds.uname, "liz")
+        self.assertEqual(self.new_creds.passwrd, "pass")
+
+
+        
+
+
