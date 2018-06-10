7080c5e3067a8ff00d9188363c9bca39135543c6
wanjiiru <liz240@live.com>
2018-06-10 18:00:27 +0300

    add more tests

--- passlocker-test.py
+++ passlocker-test.py
@@ -32,6 +32,47 @@ class Testcredential(unittest.TestCase):
         self.assertEqual(self.new_creds.passwrd, "pass")
 
 
-        
+
+
+
+
+
+
+class TestUserData(unittest.TestCase):
+    '''
+    '''
+
+
+    def setUp(self):
+        '''
+        set up structure before every test
+
+        '''
+        self.new_user_data = UserData("facebook","wanjiiru","1","pass")
+
+
+    def tearDown(self):
+        '''
+        clean up after runnong each test
+        '''
+        UserData.user_data_list = []
+
+
+
+    def test_init(self):
+        '''
+        Test for test case initialization"
+        '''
+        self.assertEqual(self.new_user_data.acc_name, "facebook")
+        self.assertEqual(self.new_user_data.acc_username, "wanjiiru")
+        self.assertEqual(self.new_user_data.acc_id , "1")
+        self.assertEqual(self.new_user_data.acc_password, "pass"
+
+
+
+
+
+
+
 
 
