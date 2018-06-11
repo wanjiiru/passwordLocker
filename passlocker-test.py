0b67561b6de79e910bf637158845d09c1634940d
wanjiiru <liz240@live.com>
2018-06-11 12:02:53 +0300

    add more tests

--- passlocker-test.py
+++ passlocker-test.py
@@ -56,7 +56,7 @@ class TestUserData(unittest.TestCase):
         set up structure before every test
 
         '''
-        self.new_user_data = UserData("facebook","wanjiiru","1","pass")
+        self.new_user_data = UserData("facebook","wanjiiru","pass")
 
 
     def tearDown(self):
@@ -73,8 +73,46 @@ class TestUserData(unittest.TestCase):
         '''
         self.assertEqual(self.new_user_data.acc_name, "facebook")
         self.assertEqual(self.new_user_data.acc_username, "wanjiiru")
-        self.assertEqual(self.new_user_data.acc_id , "1")
-        self.assertEqual(self.new_user_data.acc_password, "pass"
+        self.assertEqual(self.new_user_data.acc_password, "pass")
+
+    def test_add_password(self):
+        '''
+        Testing if the new website and password can be saved
+        '''
+        self.new_user_data.create_password()
+        self.assertEqual(len(UserData.user_data_list),1)
+
+
+
+    def test_show_data(self):
+        '''
+        Testing if the data can be displayed.
+        '''
+        self.new_user_data.create_password()
+        test_this = UserData("twitter","liz", "pass")
+        test_this.create_password()
+
+        found_user_data = UserData.show_user_data("twitter")
+        self.assertEqual(found_user_data.acc_name,test_this.acc_name)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
+        
+
+
+        
 
 
 
